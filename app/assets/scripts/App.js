634c31742e009e0bc27383ce3e00cea5a7415cfc
Jerwin <jerwinfernandez04@gmail.com>
2020-02-17 01:12:26 +0800

    completed sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
 import Modal from './modules/Modal';
 import RevealOnScroll from "./modules/RevealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
 import $ from 'jquery';
 
 
@@ -8,3 +9,5 @@ let mobileMenu = new MobileMenu();
 let modal = new Modal();
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonial'), '60%');
+
+let sticyHeader = new StickyHeader();
