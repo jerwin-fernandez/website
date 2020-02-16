51be6f508e532c20317bfebf077384818c9a2fac
Jerwin <jerwinfernandez04@gmail.com>
2020-02-17 00:49:07 +0800

    complete reveal-on-scrol

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,10 @@
 import MobileMenu from './modules/MobileMenu';
 import Modal from './modules/Modal';
+import RevealOnScroll from "./modules/RevealOnScroll";
+import $ from 'jquery';
+
 
 let mobileMenu = new MobileMenu();
 let modal = new Modal();
+new RevealOnScroll($('.feature-item'), '85%');
+new RevealOnScroll($('.testimonial'), '60%');
